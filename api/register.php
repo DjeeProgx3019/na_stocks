038a5ce163d1f150371568ae0b40d6fb14d39925
TjayAmit <108126017+DjeeProgx3019@users.noreply.github.com>
2022-11-07 16:31:53 +0800

    0

--- api/register.php
+++ api/register.php
@@ -21,23 +21,29 @@ switch($method){
         $role = $user->role;
         $status1 = $user->status1;
 
+        // $check_dept = "SELECT * FROM departments WHERE department = '$department'";
+        // $res = mysqli_query($con, $check_dept);
+        // if(mysqli_num_rows($res)){
+        //     while($row = mysqli_fetch_assoc($res)){
+        //         $PK_deptID = $row['PK_deptID'];
+        //     }
+        // }
+
         $check = "SELECT * FROM account WHERE username = '$username'";
         $checking = mysqli_query($con, $check);
   
         if (mysqli_num_rows($checking) < 1) {
             $stmt = "INSERT INTO account(PK_accID, username, `password`, usertype) VALUES ('$Pk_accID','$username','$password1','$type')";
             $results = mysqli_query($con, $stmt);
-    
-            $info = "INSERT INTO `user_info`(`PK_userID`, `PK_accID`, `PK_deptID`, `last_name`, `first_name`, `role`, `status`) VALUES ('$PK_userID','$Pk_accID','$department','$lastName','$firstName','$role','$status1')";
-            $results = mysqli_query($con, $info);
-            $data = ['status' => 1, 'message' => "Account Added Successfully!"];
+             
+            $info = "INSERT INTO user_info(PK_userID, PK_accID, PK_deptID, last_name, first_name, `role`, `status`) VALUES ('$Pk_userID', '$Pk_accID', '$department', '$lastName',' $firstName','$role','$status1')";
+            $results = mysqli_query($con, $info); 
+            
+            $data = ['status' => 1, 'message' => "Account Added Successfully!"];    
         } 
         else {
             $data = ['status' => 2, 'message' => "Failed to Create Record."];
-        }
-            
-        
-        
+        }  
         echo json_encode($data);
         break;
     }
