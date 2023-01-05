ffe474ec66c27e2284ece51f7a6c26a506d4bbd5
TjayAmit <108126017+DjeeProgx3019@users.noreply.github.com>
2023-01-05 16:53:04 +0800

    Committed

--- api/get_refrow.php
+++ api/get_refrow.php
@@ -5,13 +5,15 @@ $method=$_SERVER['REQUEST_METHOD'];
 
 switch($method){
     case 'GET':
-        $row=$con->prepare("SELECT COUNT(DISTINCT(refnum)) as 'row' from stocks WHERE date_added =?;");
-        $row->bind_param('s', $_GET['now']);
-        $row->execute(); 
-        $count = $row->get_result();
-        $count_row = $count->fetch_row();
-        $counting = $count_row[0] + 1;
-
+       
+             $row=$con->prepare("SELECT COUNT(DISTINCT (refnum)) as 'row' from stocks WHERE date_added=?");
+            // $row=$con->prepare("SELECT COUNT(*) FROM (SELECT DISTINCT (FK_userID & refnum) as 'row' from stocks WHERE date_added=?) stocks;");
+            $row->bind_param('s', $_GET['now']);
+            $row->execute(); 
+            $count = $row->get_result();
+            $count_row = $count->fetch_row();
+            $counting = $count_row[0] + 1;
+       
         echo json_encode($counting);
         break;
         mysqli_close($con);
