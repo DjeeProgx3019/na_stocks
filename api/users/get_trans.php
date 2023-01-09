6745fc6306116486e3149b8bdebeb8cacb1f5017
TjayAmit <108126017+DjeeProgx3019@users.noreply.github.com>
2023-01-09 16:55:38 +0800

    Create get_trans.php

---
+++ api/users/get_trans.php
@@ -0,0 +1,18 @@
+<?php
+include '../../config/config.php';
+
+$method=$_SERVER['REQUEST_METHOD'];
+
+switch($method){
+    case 'GET':
+        $stmt=$con->prepare("SELECT DISTINCT first_name, last_name, department, refnum, ctrlnum, itemdesc, qty, uom, unitprice, date_added, istatus FROM stocks RIGHT JOIN user_info ON stocks.FK_userID=user_info.PK_userID LEFT JOIN departments ON user_info.FK_deptID=departments.PK_deptID WHERE refnum =?;");
+        // $stmt=$con->prepare("SELECT itemdesc,qty,uom,unitprice,istatus FROM stocks where refnum =?;");
+        $stmt->bind_param('s', $_GET['item']);
+        $stmt->execute();
+        $assigned = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+
+        echo json_encode($assigned);
+        break;
+        mysqli_close($con);
+    }
+?>
