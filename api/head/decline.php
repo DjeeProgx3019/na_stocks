3fdcee89110f82a15887fbcbcccea152b9914921
TjayAmit <108126017+DjeeProgx3019@users.noreply.github.com>
2023-01-18 11:37:09 +0800

    Update

---
+++ api/head/decline.php
@@ -0,0 +1,20 @@
+<?php
+include '../../config/config.php';
+$method=$_SERVER['REQUEST_METHOD'];
+
+switch($method){
+    case 'POST': 
+        $na_stocks = json_decode(file_get_contents("php://input"));
+        $refnum = $na_stocks->refnum;
+        
+        $stmt=$con->prepare("UPDATE stocks SET `status`=2 WHERE refnum=$refnum;");
+        // $stmt->bind_param('i');
+        if($stmt->execute()){
+            $result = ['result'=> 1, 'message' => 'Data Success'];
+        }
+        echo json_encode($result);
+        break;
+        mysqli_close($con);
+}
+
+?> 
---
+++ api/head/pending.php
@@ -0,0 +1,16 @@
+<?php
+include '../../config/config.php';
+
+$method=$_SERVER['REQUEST_METHOD'];
+
+switch($method){
+    case 'GET':
+        $stmt=$con->prepare("SELECT DISTINCT refnum, first_name, last_name FROM stocks RIGHT JOIN user_info ON stocks.FK_userID=user_info.PK_userID WHERE `status` =0;");
+        $stmt->execute();
+        $assigned = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+
+        echo json_encode($assigned);
+        break;
+        mysqli_close($con);
+    }
+?>
