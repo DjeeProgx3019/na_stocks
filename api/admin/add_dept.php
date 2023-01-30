9c47c655b01cc7ac236f58a1cd0379f7fc39d6f1
TjayAmit <108126017+DjeeProgx3019@users.noreply.github.com>
2023-01-30 16:42:35 +0800

    add

--- api/admin/add_dept.php
+++ api/admin/add_dept.php
@@ -6,11 +6,11 @@ $method=$_SERVER['REQUEST_METHOD'];
 switch($method){
     case 'POST':
         $na_stocks = json_decode(file_get_contents("php://input"));
-     
+       $PK_deptID = (rand(1,100000));
         $department=$na_stocks->department;
 
-        $stmt=$con->prepare("INSERT INTO departments(department) VALUES (?)");
-        $stmt->bind_param("s",$department);
+        $stmt=$con->prepare("INSERT INTO departments(PK_deptID, department) VALUES (?,?)");
+        $stmt->bind_param("is",$PK_deptID,$department);
 
 
         if($stmt->execute()){
